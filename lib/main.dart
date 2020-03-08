5e7ff998bae287ee6cef00c6948cb602e0917329
srjpnd <srjpnd96@gmail.com>
2020-03-08 22:16:53 +0530

    home change has beem done

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: " business card",
+  theme: ThemeData(),
+  home: ui(),
+
+));
+
+class ui extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
